e45967c07898ce44cd66ff478d03599a90b12b7a
Anthony Goh <anthony_tonz@ymail.com>
2019-12-11 13:07:22 +0700

    Favicon and Title for Admin Panel

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/ico" href="https://dilenium.com/administratoronly/assets/images/favicon (2).ico "/>
 
     {{-- Encrypted CSRF token for Laravel, in order for Ajax requests to work --}}
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <title>
-      The Finard
+      The Finard - Admin Panel
     </title>
 
     @yield('before_styles')
