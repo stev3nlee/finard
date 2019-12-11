3e9a4485f643e12fabe95e9af0ac63faaafec5d2
steven <steven@idsskincare.com>
2019-12-11 13:18:28 +0700

    error 404 redirect

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,26 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception))
+        {
+            switch (intval($exception->getStatusCode())) {
+                // not found
+                case 404:
+                    return redirect('/');
+                    break;
+                // internal error
+                case 500:
+                    return \Response::view('custom.500',array(),500);
+                    break;
+
+                default:
+                    return $this->renderHttpException($exception);
+                    break;
+            }
+        }
+        else
+        {
+            return parent::render($request, $exception);
+        }
     }
 }
